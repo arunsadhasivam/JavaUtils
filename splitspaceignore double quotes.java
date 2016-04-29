d3a98a82fa8b3c36954dc9f229a0e43a25d22d09
arun <arunsadhasivam@yahoo.co.in>
2016-04-29 15:26:18 +0530

    Update splitspaceignore double quotes.java

--- splitspaceignore double quotes.java
+++ splitspaceignore double quotes.java
@@ -2,6 +2,10 @@ public class Test {
 
 	public static void main(String args[]) {
 		String line = "1000, 10,  \"value header\", space ";
+		
+		String arr[] = line.split(",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
+		System.out.println(arr.length);
+		
 		String result[] = line.replaceAll("^\"", "").split("\"?(,|$)(?=(([^\"]*\"){2})*[^\"]*$) *\"?", -1);
 		for (String token : result) {
 			System.out.println("Test.main()" + token);
